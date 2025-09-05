7b86c59041ca1742648df4b02b711c6fe3716b09
neerajgulia <contactus@opsworld.in>
2025-09-05 12:21:42 +0530

    editedtfcode

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 resource "aws_s3_bucket" "bucket1" {
   bucket = var.bucket_name
+
   tags = {
     Environment = "Dev"
   }
 }
 
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.bucket1.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 resource "aws_s3_bucket" "bucket2" {
   bucket = "my-tf-test-bucket2-manisha"
   tags = {
